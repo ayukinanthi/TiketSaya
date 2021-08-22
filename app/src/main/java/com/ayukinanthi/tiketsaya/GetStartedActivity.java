cf25ae4091eab7a6287ce9c41f0fc9eaca236a85
Putri <aiyukape@gmail.com>
2021-08-22 16:21:55 +0700

    pembuatan register one activity dan penambahan string, color

--- app/src/main/java/com/ayukinanthi/tiketsaya/GetStartedActivity.java
+++ app/src/main/java/com/ayukinanthi/tiketsaya/GetStartedActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class GetStartedActivity extends AppCompatActivity {
 
     Button btn_sign_in;
+    Button btn_new_account_create;
 
 
     @Override
@@ -19,13 +20,24 @@ public class GetStartedActivity extends AppCompatActivity {
 
         //definisi
         btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_new_account_create = findViewById(R.id.btn_new_acc_create);
+
         //pas lagi di klik
+        //sign in
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent gotosign = new Intent(GetStartedActivity.this, SignInActivity.class);
                 startActivity(gotosign);
 
+            }
+        });
+        //new account
+        btn_new_account_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoregisterone = new Intent(GetStartedActivity.this, RegisterOneActivity.class);
+                startActivity(gotoregisterone);
             }
         });
     }
--- app/src/main/java/com/ayukinanthi/tiketsaya/SignInActivity.java
+++ app/src/main/java/com/ayukinanthi/tiketsaya/SignInActivity.java
@@ -2,13 +2,29 @@ package com.ayukinanthi.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    TextView btn_new_acc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btn_new_acc = findViewById(R.id.btn_new_acc);
+        //setOnClickListener memberikan fungsi pada tiap btn_new_acc
+        btn_new_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoregisterone = new Intent(SignInActivity.this, RegisterOneActivity.class);
+                startActivity(gotoregisterone);
+
+            }
+        });
     }
 }
